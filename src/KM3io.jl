901ab95e54f4e0167a593419556a252bb3afbc32
Johannes Schumann <johannes.schumann@fau.de>
2020-01-13 18:51:07 +0100

    Evt writer (without header and not all tags yet)

--- src/KM3io.jl
+++ src/KM3io.jl
@@ -39,7 +39,7 @@ struct Hit <: AbstractHit
 end
 
 struct KineticInfo
-    vtx_pos::Cartesian{T} where {T <: Real}
+    pos::Cartesian{T} where {T <: Real}
     dir::Cartesian{T} where {T <: Real}
     energy::Real
     time::Real
@@ -89,6 +89,12 @@ function read_evt_hit(line::AbstractString)
     Hit(id, pmt_id, npe, time, geantid, track_id)
 end
 
+function build_evt_hit_entry(hit::Hit)
+    retval =    "hit: $(hit.id) $(hit.PMid) $(hit.npe) "
+    retval *=   "$(hit.time) $(hit.particle.value) $(hit.track_id) \n"
+    return retval
+end
+
 function parse_kin(fields::Vector{T}) where {T <: AbstractString}
     x = parse(Float64, fields[1])
     y = parse(Float64, fields[2])
@@ -116,6 +122,16 @@ function read_evt_neutrino(line::AbstractString)
     Neutrino(id, kin, bjorken, ichan, particle, cc)
 end
 
+function build_evt_nu_entry(nu::Neutrino)
+    retval =    "neutrino: $(nu.id) "
+    retval *=   "$(nu.kin.pos.x) $(nu.kin.pos.y) $(nu.kin.pos.z) "
+    retval *=   "$(nu.kin.dir.x) $(nu.kin.dir.y) $(nu.kin.dir.z) "
+    retval *=   "$(nu.kin.energy) $(nu.kin.time) "
+    retval *=   "$(nu.bjorken[1]) $(nu.bjorken[2]) "
+    retval *=   "$(nu.ichannel) $(nu.particle.value) $(convert(Int, nu.cc))\n"
+    return retval
+end
+
 function read_evt_track(line::AbstractString)
     fields = split(line)
     id = parse(Int64, fields[2])
@@ -124,12 +140,19 @@ function read_evt_track(line::AbstractString)
     Track(id, kin, particle)
 end
 
+function build_evt_track_entry(trk::Track)
+    retval =    "track_in: $(trk.id) "
+    retval *=   "$(trk.kin.pos.x) $(trk.kin.pos.y) $(trk.kin.pos.z) "
+    retval *=   "$(trk.kin.dir.x) $(trk.kin.dir.y) $(trk.kin.dir.z) "
+    retval *=   "$(trk.kin.energy) $(trk.kin.time) $(trk.particle.value) \n"
+    return retval
+end
+
 function read_evt_nu_weights(line::AbstractString)
     fields = split(line)
     weights = parse.(Float64, fields[2:4])
     NeutrinoWeights{Float64}(weights..., 0)
 end
-
 
 function read_evt_file(filepath::AbstractString)
     f = open(filepath)
@@ -176,6 +199,29 @@ function read_compound(files::Vector{T}) where {T <: AbstractString}
         end
     end
     filedata
+end
+
+function write_evt_file(filepath::AbstractString, data::EvtFile)
+    f = open(filepath, "w")
+    for key in sort(collect(keys(data.events)))
+        event = data.events[key]
+        write(f, "start_event: $key 1\n")
+        for hit in event.hits
+            hit_line = build_evt_hit_entry(hit)
+            write(f, hit_line)
+        end
+        write(f, "total_hits: $(length(event.hits))\n")
+        for track in event.tracks
+            trk_line = build_evt_track_entry(track) 
+            write(f, trk_line)
+        end
+        if !ismissing(event.neutrino)
+            nu_line = build_evt_nu_entry(event.neutrino) 
+            write(f, nu_line)
+        end
+        write(f, "end_event: \n")
+    end
+    close(f)
 end
 
 end # module
