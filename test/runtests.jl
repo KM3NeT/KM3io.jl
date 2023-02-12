64ed53d2d88ddccf43472c93102919347dc58a91
Tamas Gal <himself@tamasgal.com>
2023-02-12 13:57:17 +0100

    Cosmetics in test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,7 @@
 using Test
 
-@testset "KM3io.jl" begin
-    include("root.jl")
-    include("daq.jl")
-    include("tools.jl")
-    include("hardware.jl")
-    include("acoustics.jl")
-end
+include("root.jl")
+include("daq.jl")
+include("tools.jl")
+include("hardware.jl")
+include("acoustics.jl")
