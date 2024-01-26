89898e5574392da715fa65ed7e3f4d668a61e366
Tamas Gal <himself@tamasgal.com>
2024-01-26 14:59:56 +0100

    Add getindex

--- src/tools/helpers.jl
+++ src/tools/helpers.jl
@@ -6,10 +6,13 @@ struct RBRIterator
     f::ROOTFile
 end
 Base.length(itr::RBRIterator) = length(itr.f.online.events)
-function Base.iterate(itr::RBRIterator, state=1)
-    state > length(itr) && return nothing
-    event = itr.f.online.events[state]
+function Base.getindex(itr::RBRIterator, idx::Integer)
+    event = itr.f.online.events[idx]
     mc_idx = event.header.trigger_counter + 1
     mc_event = itr.f.offline[mc_idx]
-    ((event, mc_event), state + 1)
+    (event, mc_event)
+end
+function Base.iterate(itr::RBRIterator, state=1)
+    state > length(itr) && return nothing
+    (itr[state], state + 1)
 end
