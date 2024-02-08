0a96ee265c876c57d276e199c8c7e598d61c81cb
Tamas Gal <himself@tamasgal.com>
2024-02-08 15:02:14 +0100

    Fix angle tests

--- test/tools.jl
+++ test/tools.jl
@@ -330,8 +330,8 @@ end
 
 
 @testset "math" begin
-    @test 0 == angle([1,0,0], [1,0,0])
-    @test π/2 ≈ angle([1,0,0], [0,1,0])
-    @test π/2 ≈ angle([1,0,0], [0,0,1])
-    @test π ≈ angle([1,0,0], [-1,0,0])
+    @test 0 == angle(Direction(1.,0,0), Direction(1.,0,0))
+    @test π/2 ≈ angle(Direction(1.,0,0), Direction(0.,1,0))
+    @test π/2 ≈ angle(Direction(1.,0,0), Direction(0.,0,1))
+    @test π ≈ angle(Direction(1.,0,0), Direction(-1.,0,0))
 end
