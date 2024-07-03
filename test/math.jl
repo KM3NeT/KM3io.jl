34e4d8269fcc5736d9be64b678c634c51c3ff753
Tamas Gal <himself@tamasgal.com>
2024-07-03 11:26:50 +0200

    Fix normalisation in tests

--- test/math.jl
+++ test/math.jl
@@ -13,11 +13,11 @@ using Test
     # should normalise internally
     q1 = [0.4, 0.0]
     q2 = [0.0, 0.9]
-    @test [1.0, 0.0] ≈ slerp(q1, q2, 0)
-    @test [0.0, 1.0] ≈ slerp(q1, q2, 1)
-    @test [0.9510565162951538, 0.30901699437494745] ≈ slerp(q1, q2, 0.2)
-    @test [0.70710678, 0.70710678] ≈ slerp(q1, q2, 0.5)
-    @test [0.45399049973954686, 0.8910065241883678] ≈ slerp(q1, q2, 0.7)
+    @test [1.0, 0.0] ≈ slerp(q1, q2, 0; normalized=false)
+    @test [0.0, 1.0] ≈ slerp(q1, q2, 1; normalized=false)
+    @test [0.9510565162951538, 0.30901699437494745] ≈ slerp(q1, q2, 0.2; normalized=false)
+    @test [0.70710678, 0.70710678] ≈ slerp(q1, q2, 0.5; normalized=false)
+    @test [0.45399049973954686, 0.8910065241883678] ≈ slerp(q1, q2, 0.7; normalized=false)
 
     q1 = [1.0, 0.0, 0.0]
     q2 = [0.0, 0.0, 1.0]
