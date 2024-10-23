c66852dcd83894176d135ce094b0579e82914442
Tamas Gal <himself@tamasgal.com>
2024-10-23 14:01:23 +0200

    Extend the Detector constructor

--- ext/KM3ioKM3DBExt.jl
+++ ext/KM3ioKM3DBExt.jl
@@ -8,11 +8,24 @@ else
 end
 
 """
+    Detector(det_id::Integer; kwargs...)
+
 Instantiate a detector by polling the database for a given detector ID.
 The keyword arguments `kwargs` are passed to the `detx()` function in `KM3DB.jl`.
 """
 function Detector(det_id::Integer; kwargs...)
     raw = detx(det_id; kwargs...)
+    read_detx(IOBuffer(raw))
+end
+
+"""
+    Detector(det_id::Integer, run_id::Integer; version=5)
+
+Instantiate a detector which was used for a given detector to take the
+specified run by polling the database.
+"""
+function Detector(det_id::Integer, run_id::Integer; version=5)
+    raw = detx(det_id, run_id; version=version)
     read_detx(IOBuffer(raw))
 end
 
