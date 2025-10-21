e0211d5b545a85074a370f0a9987a2e05f3b4f06
Tamas Gal <himself@tamasgal.com>
2025-10-21 09:05:49 +0200

    Add footprint example to docs

--- docs/make.jl
+++ docs/make.jl
@@ -27,6 +27,7 @@ makedocs(;
             "examples/online_data.md",
             "examples/offline_data.md",
             "examples/cherenkov_times.md",
+            "examples/footprint.md",
             "examples/orientations.md",
             "examples/controlhost.md",
             "examples/hdf5.md",
